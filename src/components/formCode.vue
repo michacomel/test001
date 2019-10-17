db1db568fcf8d71111da536e11fb252150547350
michacomel <amirashahrizan96@gmail.com>
2019-10-17 12:30:08 +0800

    added vuetify

---
+++ src/components/formCode.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+  <v-app id="inspire">
+    <v-container id="dropdown-example-1">
+      <v-overflow-btn
+        class="my-2"
+        :items="dropdown_font"
+        label="Overflow Btn"
+        target="#dropdown-example-1"
+      ></v-overflow-btn>
+    </v-container>
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
